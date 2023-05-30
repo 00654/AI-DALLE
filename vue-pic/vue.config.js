1f9d2b82bf612c4f874e01edccd868e8a61518d3
root <root@vultr.guest>
2023-05-30 22:24:25 +0800

    fix bug

--- vue-pic/vue.config.js
+++ vue-pic/vue.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     open: true, // 配置项目在启动时自动在浏览器打开
     proxy: {
         '/api' : { // '/api'是代理标识，一般是每个接口前的相同部分
-            target: "http://xx.xx.xx.xx:xx/", // 请求地址，一般是服务器地址
+            target: "http:/xx.xx.xx.xx:1234/", // 请求地址，一般是服务器地址
             changeOrigin: true, // 是否进行跨域
             pathRewrite: { // pathRewrite的作用是把请求接口中的 '/api'替换掉，一般是替换为空""
                 '^/api':""
